c4abd9c19d3683a0990fc0f497f010e27964241a
Danny Arends <Danny.Arends@gmail.com>
2021-05-14 17:22:05 +0200

    Adding the GPL-v3 license

--- src/math/quaternion.d
+++ src/math/quaternion.d
@@ -1,3 +1,7 @@
+// Copyright Danny Arends 2021
+// Distributed under the GNU General Public License, Version 3
+// See accompanying file LICENSE.txt or copy at https://www.gnu.org/licenses/gpl-3.0.en.html
+
 import vector : x,y,z;
 
 /* Create a T[3], w as a T[4] */
