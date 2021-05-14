32a6733741404c11b2f99e8217b89a99d1b595cc
Danny Arends <Danny.Arends@gmail.com>
2021-05-14 11:10:00 +0200

    First of many SDL2.0.14 adapted project files

---
+++ app/jni/Application.mk
@@ -0,0 +1,10 @@
+
+# Uncomment this if you're using STL in your project
+# You can find more information here:
+# https://developer.android.com/ndk/guides/cpp-support
+# APP_STL := c++_shared
+
+APP_ABI := arm64-v8a
+
+# Min runtime API level
+APP_PLATFORM=android-16
