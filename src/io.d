f1962f220347f6683e3b0d998001cf2ecfbb3f05
Danny Arends <Danny.Arends@gmail.com>
2021-05-19 08:29:30 +0200

    Minimizing code, use toStdout

--- src/io.d
+++ src/io.d
@@ -11,22 +11,21 @@ import calderad, buffer, wavefront;
 uint[] readFile(string path) {
   SDL_RWops *rw = SDL_RWFromFile(toStringz(path), "rb");
   if (rw == null){
-    SDL_Log("readFile: couldn't open file '%s'\n", toStringz(path));
+    toStdout("readFile: couldn't open file '%s'\n", toStringz(path));
     return [];
   }
   uint[] res;
-  auto res_size = SDL_RWsize(rw);
 
-  res.length = cast(size_t)res_size;
+  res.length = cast(size_t)SDL_RWsize(rw);
   size_t nb_read_total = 0, nb_read = 1;
   uint* cpos = &res[0];
-  while (nb_read_total < res_size && nb_read != 0) {
-    nb_read = SDL_RWread(rw, cpos, 1, cast(size_t)(res_size - nb_read_total));
+  while (nb_read_total < res.length && nb_read != 0) {
+    nb_read = SDL_RWread(rw, cpos, 1, cast(size_t)(res.length - nb_read_total));
     nb_read_total += nb_read;
     cpos += nb_read;
   }
   SDL_RWclose(rw);
-  if (nb_read_total != res_size) SDL_Log("readFile: loaded %db, expected %db\n", nb_read_total, res_size);
-  SDL_Log("Loaded %d bytes\n", nb_read_total);
+  if (nb_read_total != res.length) toStdout("readFile: loaded %db, expected %db\n", nb_read_total, res.length);
+  toStdout("Loaded %d bytes\n", nb_read_total);
   return res;
 }
