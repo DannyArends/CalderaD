a04ce794e6b664636591d222e5a76a882871ef56
Danny Arends <Danny.Arends@gmail.com>
2021-06-13 09:29:31 +0200

    Geometry draw() function, commands.d now under 100 loc

--- src/commands.d
+++ src/commands.d
@@ -2,7 +2,7 @@
 // Distributed under the GNU General Public License, Version 3
 // See accompanying file LICENSE.txt or copy at https://www.gnu.org/licenses/gpl-3.0.en.html
 
-import calderad, vertex, matrix, pushconstant;
+import calderad, geometry, vertex, matrix, pushconstant;
 
 void createCommandPool(ref App app) {
   VkCommandPoolCreateInfo poolInfo = {
@@ -35,13 +35,13 @@ void createCommandPool(ref App app) {
 }
 
 @nogc void endSingleTimeCommands(ref App app, VkCommandBuffer commandBuffer) nothrow {
-    vkEndCommandBuffer(commandBuffer);
+  vkEndCommandBuffer(commandBuffer);
 
-    VkSubmitInfo submitInfo = {sType: VK_STRUCTURE_TYPE_SUBMIT_INFO, commandBufferCount: 1, pCommandBuffers: &commandBuffer };
-    vkQueueSubmit(app.graphicsQueue, 1, &submitInfo, VK_NULL_ND_HANDLE);
-    vkQueueWaitIdle(app.graphicsQueue);
+  VkSubmitInfo submitInfo = {sType: VK_STRUCTURE_TYPE_SUBMIT_INFO, commandBufferCount: 1, pCommandBuffers: &commandBuffer };
+  vkQueueSubmit(app.graphicsQueue, 1, &submitInfo, VK_NULL_ND_HANDLE);
+  vkQueueWaitIdle(app.graphicsQueue);
 
-    vkFreeCommandBuffers(app.device, app.commandPool, 1, &commandBuffer);
+  vkFreeCommandBuffers(app.device, app.commandPool, 1, &commandBuffer);
 }
 
 
@@ -90,28 +90,8 @@ void createCommandBuffers(ref App app) {
     vkCmdBeginRenderPass(app.commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
     toStdout("Render pass recording to %d", i);
     vkCmdBindPipeline(app.commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, app.pipeline.graphicsPipeline);
-    
     for(size_t j = 0; j < app.geometry.length; j++) {
-      VkBuffer[] vertexBuffers = [app.geometry[j].vertexBuffer];
-      VkDeviceSize[] offsets = [0];
-
-      PushConstant pc = {
-        oId: to!int(j),
-        tId: app.geometry[j].texture,
-        model: app.geometry[j].filemodel
-      };
-      vkCmdPushConstants(app.commandBuffers[i], app.pipeline.pipelineLayout, 
-                         VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, 
-                         PushConstant.sizeof, &pc);
-
-      vkCmdBindVertexBuffers(app.commandBuffers[i], VERTEX_BUFFER_BIND_ID, 1, &app.geometry[j].vertexBuffer, &offsets[0]);
-      vkCmdBindVertexBuffers(app.commandBuffers[i], INSTANCE_BUFFER_BIND_ID, 1, &app.geometry[j].instanceBuffer, &offsets[0]);
-
-      vkCmdBindIndexBuffer(app.commandBuffers[i], app.geometry[j].indexBuffer, 0, VK_INDEX_TYPE_UINT32);
-
-      vkCmdBindDescriptorSets(app.commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, app.pipeline.pipelineLayout, 0, 1, &app.descriptor.descriptorSets[app.geometry[j].texture], 0, null);
-
-      vkCmdDrawIndexed(app.commandBuffers[i], cast(uint)app.geometry[j].indices.length, cast(uint)app.geometry[j].instances.length, 0, 0, 0);
+      app.draw(i, j);
     }
     vkCmdEndRenderPass(app.commandBuffers[i]);
     enforceVK(vkEndCommandBuffer(app.commandBuffers[i]));
--- src/obj/geometry.d
+++ src/obj/geometry.d
@@ -2,13 +2,14 @@
 // Distributed under the GNU General Public License, Version 3
 // See accompanying file LICENSE.txt or copy at https://www.gnu.org/licenses/gpl-3.0.en.html
 
-import calderad, vector, matrix, vertex;
+import calderad, vector, matrix, pushconstant, vertex;
 
 struct GeometryInstanceData { // Holds instance specific offset data
   mat4 offset = mat4.init;
 };
 
 struct Geometry {
+  // Vulkan vertex, index and indices bufferhandles
   VkBuffer vertexBuffer = VK_NULL_ND_HANDLE;
   VkDeviceMemory vertexBufferMemory = VK_NULL_ND_HANDLE;
 
@@ -18,12 +19,37 @@ struct Geometry {
   VkBuffer instanceBuffer = VK_NULL_ND_HANDLE;
   VkDeviceMemory instanceBufferMemory = VK_NULL_ND_HANDLE;
 
+  // Push constants
+  int texture = 0;  // Texture
+  mat4 filemodel = mat4.init; // Model is there to correct for file format up/orientation differences (3DS, MTL, WaveFront)
+
+  // Vertices, indices and instances
   Vertex[] vertices;
   uint[] indices;
   GeometryInstanceData[] instances = [GeometryInstanceData.init]; 
 
-  int texture = 0;
-  mat4 filemodel = mat4.init; // Model is there to correct for file format up/orientation differences (3DS, MTL, WaveFront)
+  Geometry* next; // TODO: unused
+}
 
-  Geometry* next;
+// Draws geometry[j] to buffer[i]
+void draw(ref App app, size_t i, size_t j) {
+  VkDeviceSize[] offsets = [0];
+
+  PushConstant pc = {
+    oId: to!int(j),
+    tId: app.geometry[j].texture,
+    model: app.geometry[j].filemodel
+  };
+  vkCmdPushConstants(app.commandBuffers[i], app.pipeline.pipelineLayout, 
+                     VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, 
+                     PushConstant.sizeof, &pc);
+
+  vkCmdBindVertexBuffers(app.commandBuffers[i], VERTEX_BUFFER_BIND_ID, 1, &app.geometry[j].vertexBuffer, &offsets[0]);
+  vkCmdBindVertexBuffers(app.commandBuffers[i], INSTANCE_BUFFER_BIND_ID, 1, &app.geometry[j].instanceBuffer, &offsets[0]);
+
+  vkCmdBindIndexBuffer(app.commandBuffers[i], app.geometry[j].indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+
+  vkCmdBindDescriptorSets(app.commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, app.pipeline.pipelineLayout, 0, 1, &app.descriptor.descriptorSets[app.geometry[j].texture], 0, null);
+
+  vkCmdDrawIndexed(app.commandBuffers[i], cast(uint)app.geometry[j].indices.length, cast(uint)app.geometry[j].instances.length, 0, 0, 0);
 }
