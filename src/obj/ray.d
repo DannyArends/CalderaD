9f3dfa6aad5314677621d34603e4de0ca1991934
Danny Arends <Danny.Arends@gmail.com>
2021-09-26 13:26:43 +0200

    fix import bug

--- src/obj/ray.d
+++ src/obj/ray.d
@@ -2,7 +2,7 @@
 // Distributed under the GNU General Public License, Version 3
 // See accompanying file LICENSE.txt or copy at https://www.gnu.org/licenses/gpl-3.0.en.html
 
-import calderad;
+import calderad, geometry;
 
 struct Ray { // TODO implement the Ray structure
   Geometry geometry = { };
